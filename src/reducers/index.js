be169aa28894468f4d4d7cccaf29b225762bdbfc
Bruce Reif <bruce.reif@peanutlabs.com>
2017-10-07 18:16:23 -0700

    update letter tests, add flow to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+// @flow
 import { combineReducers } from "redux";
 import letters from "./letters";
 import words from "./words";
--- src/reducers/letters.spec.js
+++ src/reducers/letters.spec.js
@@ -4,12 +4,43 @@ import { INIT_LETTERS, PUSH_LETTER, POP_LETTER } from "../action-types";
 
 describe("letters reducer", () => {
 	const initialState = deepFreeze({
-		inertLetters: ["T", "E", "S", "T"],
-		activeLetters: ["I", "N", "G"]
+		lettersById: {
+			0: {
+				id: "0",
+				glyph: "T"
+			},
+			1: {
+				id: "1",
+				glyph: "E"
+			},
+			2: {
+				id: "2",
+				glyph: "S"
+			},
+			3: {
+				id: "3",
+				glyph: "T"
+			},
+			4: {
+				id: "4",
+				glyph: "I"
+			},
+			5: {
+				id: "5",
+				glyph: "N"
+			},
+			6: {
+				id: "6",
+				glyph: "G"
+			}
+		},
+		inertLetters: [0, 1, 2, 3],
+		activeLetters: [4, 5, 6]
 	});
 
 	it("handles initial state", () => {
 		expect(letters()).toEqual({
+			lettersById: {},
 			inertLetters: [],
 			activeLetters: []
 		});
@@ -23,27 +54,55 @@ describe("letters reducer", () => {
 		const payload = ["J", "Q", "U", "E", "R", "Y"];
 		const action = { type: INIT_LETTERS, payload };
 		const expectedState = {
-			inertLetters: payload,
+			lettersById: {
+				0: {
+					id: "0",
+					glyph: "J"
+				},
+				1: {
+					id: "1",
+					glyph: "Q"
+				},
+				2: {
+					id: "2",
+					glyph: "U"
+				},
+				3: {
+					id: "3",
+					glyph: "E"
+				},
+				4: {
+					id: "4",
+					glyph: "R"
+				},
+				5: {
+					id: "5",
+					glyph: "Y"
+				}
+			},
+			inertLetters: ["0", "1", "2", "3", "4", "5"],
 			activeLetters: []
-		}
+		};
 		expect(letters(initialState, action)).toEqual(expectedState);
-	})
+	});
 
 	it("handles PUSH_LETTER", () => {
-		const action = { type: PUSH_LETTER, payload: "T" };
+		const action = { type: PUSH_LETTER, payload: 0 };
 		const expectedState = {
-			inertLetters: ["E", "S", "T"],
-			activeLetters: ["I", "N", "G", "T"]
+			...initialState,
+			inertLetters: [1, 2, 3],
+			activeLetters: [4, 5, 6, 0]
 		};
 		expect(letters(initialState, action)).toEqual(expectedState);
 	});
 
 	it("handles POP_LETTER", () => {
-		const action = { type: POP_LETTER };
+		const action = { type: POP_LETTER, payload: 6 };
 		const expectedState = {
-			inertLetters: ["T", "E", "S", "T", "G"],
-			activeLetters: ["I", "N"]
-		}
+			...initialState,
+			inertLetters: [6, 0, 1, 2, 3],
+			activeLetters: [4, 5]
+		};
 		expect(letters(initialState, action)).toEqual(expectedState);
 	});
 });
