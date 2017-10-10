587eac496ae450bf911ea03ad77111dc4b0f06d0
Bruce Reif <bruce.reif@peanutlabs.com>
2017-10-09 18:05:47 -0700

    add react motion, some styles

--- src/components/LetterLists.js
+++ src/components/LetterLists.js
@@ -2,6 +2,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Letter from "./Letter";
+import { Motion } from "react-motion";
 import type { LettersById, LetterId } from "../types";
 
 type Props = {
@@ -10,19 +11,47 @@ type Props = {
 	activeLetters: Array<LetterId>
 };
 
+const layout = (index, active) => {
+	const x = index * 100;
+	const y = active ? 0 : 100;
+	return [x, y];
+};
+
 class LetterLists extends React.Component<Props> {
 	render() {
 		const { lettersById, inertLetters, activeLetters } = this.props;
-		const inertChildren = inertLetters.map(id => (
-			<Letter key={id} id={id} glyph={lettersById[id].glyph} />
-		));
+		const inertChildren = inertLetters.map((id, index) => {
+			let [x, y] = layout(index, false);
+			let style = {
+				translateX: x,
+				translateY: y
+			};
+			return (
+				<Motion key={id} style={style}>
+					{({ translateX, translateY }) => {
+						return (
+							<Letter
+								id={id}
+								glyph={lettersById[id].glyph}
+								style={{
+									transform: `translate3d(${translateX}px, ${translateY}px`
+								}}
+							/>
+						);
+					}}
+				</Motion>
+			);
+			return;
+		});
 		const activeChildren = activeLetters.map(id => (
 			<Letter key={id} id={id} glyph={lettersById[id].glyph} />
 		));
 		return (
 			<div className="LetterLists">
-				<div className="ActiveLetters">{activeChildren}</div>
-				<div className="InertLetters">{inertChildren}</div>
+				{activeChildren}
+				{inertChildren}
+				<div className="ActiveLetters" />
+				<div className="InertLetters" />
 			</div>
 		);
 	}
@@ -31,7 +60,7 @@ class LetterLists extends React.Component<Props> {
 LetterLists.propTypes = {
 	lettersById: PropTypes.objectOf(PropTypes.object).isRequired,
 	inertLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
-	activeLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+	activeLetters: PropTypes.arrayOf(PropTypes.string).isRequired
+};
 
 export default LetterLists;
